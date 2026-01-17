2d8e0ce58008f401d085c695ef82356905f62d22
MatiiiBL <mati20asd@gmail.com>
2026-01-17 09:10:33 -0500

    Created the route paths to the Pages I created

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import ProjectsPage from './pages/ProjectsPage.jsx'
+import AboutPage from './pages/AboutPage.jsx'
+import ScrollToTop from './components/ScrollToTop.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-
-    
-
+    <HashRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )
